afbbb5aa19805f26374aa0f9be0a1bf89708ef56
sid mukkamala <siddharth.world@gmail.com>
2022-06-11 23:28:54 +0200

    add link

--- euler.hs
+++ euler.hs
@@ -3,6 +3,8 @@ import Control.Monad (guard)
 -- Warmup problems 
 
 -- Problem 1, unnecessarily using do-noation while a list comprehension would "do" 
+-- https://projecteuler.net/problem=1
+
 problem001 :: Int  -> Int 
 problem001 a = let lstgen = do 
                               val <- [1..a]
