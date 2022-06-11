3a8de42fd2bef6cb6f892a16253a4a4434f1ee3e
sid mukkamala <siddharth.world@gmail.com>
2022-06-11 23:22:08 +0200

    warmup problem 1

---
+++ euler.hs
@@ -0,0 +1,14 @@
+import Control.Monad (guard)
+
+-- Warmup problems 
+
+-- Problem 1, unnecessarily using do-noation while a list comprehension would "do" 
+problem001 :: Int  -> Int 
+problem001 a = let lstgen = do 
+                              val <- [1..a]
+                              guard $ (mod val 3 == 0) || (mod val 5 == 0) 
+                              return val
+                in sum lstgen
+
+
+-- Problem 432
