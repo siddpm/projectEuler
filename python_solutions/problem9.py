dc26933b7328b9900ebc1f642e9b9e49c5c977dd
SidMukkamala <siddharth.world@gmail.com>
2025-05-23 22:56:06 +0200

    optimising speacial pythagorean triplet solution, performs at 1000 times improvement

--- python_solutions/problem9.py
+++ python_solutions/problem9.py
@@ -1,3 +1,5 @@
+from time  import time
+
 def special_pythagorean_triplet():
     for a in range(1,999):
         for b in range(a, 1000):
@@ -11,4 +13,18 @@ def is_pythagorean_triplet(a,b,c):
         return True
     return False
 
+def optimised_special_pythagorean_triplet():
+    for a in range(333,3,-1):
+        for b in range(a, round((1000 - a)/2)):
+            c_squared = (1000 - a - b)**2
+            if a**2 + b**2 == c_squared:
+                return a*b*(1000-a-b)
+
+start = time()
 print(special_pythagorean_triplet())
+
+print(f"brute force took: {time() - start}")
+
+start = time()
+print(optimised_special_pythagorean_triplet())
+print(f"Optimised took: {time() - start}")
