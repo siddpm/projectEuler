9fa0a6eec833019a3dde14e16b46adc86c4f6a7c
SidMukkamala <siddharth.world@gmail.com>
2025-05-29 16:36:26 +0200

    optimised the generation of primes by using the fundamental theorem of arthmetic

--- python_solutions/helpers.py
+++ python_solutions/helpers.py
@@ -1,10 +1,10 @@
 """ Helper functions for solving Project Euler problems """
 
 import math
-from typing import Generator, Dict
+from typing import Generator, Dict, List
 
 
-def is_prime(n: int) -> bool:
+def is_prime(n: int, primes: List[int]) -> bool:
 
     # Edge case : 2 is prime by definition
     if n == 1:
@@ -18,25 +18,36 @@ def is_prime(n: int) -> bool:
         # and d is a factor of n, such that n/d = r (r is a whole number)
         # From our assumptions, r > sqrt(n) (otherwise, we would've already found it)
         # But then the factor r*d must necessarily be > n. This contradicts our assumption. QED
+    
+    # Additionally, as all composite numbers can be factored into prime (fundamental theorem of arithmetic)
+    # we only need to compare against previous primes below sqrt(n)
     sqrt_n = math.isqrt(n)
 
-    for i in range(2, sqrt_n + 1):
-        if n % i == 0:
+    # for i in range(2, sqrt_n + 1):
+    #     if n % i == 0:
+    #         return False
+    for prime in primes:
+        if prime > sqrt_n:
+            break
+        if n % prime == 0:
             return False
     return True
 
 
 def prime_gen() -> Generator[int, None, None]:
 
+    # keep a list of computed primes for reuse
+    primes = [2]
+
     # Starting candidate by prime definiton
-    candidate = 2
-    yield candidate
+    yield primes[0]
 
     # We now only look at odd numbers as candidates for primality testing
     candidate = 3
 
     while True:
-        if is_prime(candidate):
+        if is_prime(candidate, primes):
+            primes.append(candidate)
             yield candidate
         candidate += 2
 
@@ -74,6 +85,10 @@ def prime_factors(n: int) -> Dict[int, int]:
     28 % 2 == 0 -> 2 is a factor, number reduced to 28/2 = 14
     14 % 2 == 0 -> 2 repeats again so we record an exponent of 2 and etc
     '''
+
+    # edge case
+    if n == 1:
+        return {1:1}
 
     # store prime factors and their exponents
     prime_factors = {}
--- python_solutions/problem12.py
+++ python_solutions/problem12.py
@@ -1,6 +1,6 @@
 from functools import reduce
 from helpers import prime_factors
-
+from time import time
 
 def brute_force():
     k = 2
@@ -28,6 +28,14 @@ def brute_force():
         
         k += 1
 
+def total_factor_calculator(prime_factors_dict):
+     # From a number's prime factors and their exponents, we can calculate all factors
+     # See brute force solution for an explanation of the following code. 
+     prime_factor_exponents = [value + 1 for _,value in prime_factors_dict.items()]
+     total_factors = reduce(lambda x,y: x*y, prime_factor_exponents)
+     return total_factors
+
+
 
 def highly_divisible_triangular_number():
         k = 2
@@ -35,19 +43,23 @@ def highly_divisible_triangular_number():
         while True:
             # Compute Triangular number values
             # Using formula Tn = n(n+1)/2  (this is the sum of series 1 + 2 + 3 +  4 + ... + n)
+
+            # we make an optimisation here: n and n+1 are coprime, so total_factors can of Tn can be written as 
+            # if n is even: factors(n/2)* factors(n+1)
+            # if n is odd: factors((n + 1)/2) + factors(n)
+
             tn = (k*(k + 1))//2
 
-            # Retireve all prime factors of tn 
-            prime_factors_dict = prime_factors(tn)
-                    
+            if k % 2 == 0:
+                 prime_factors_even = prime_factors(k//2)
+                 prime_factor_odd = prime_factors(k + 1)
+
+            else:
+                 prime_factors_even = prime_factors((k + 1)//2)
+                 prime_factor_odd = prime_factors(k)
             
-            # Now that we know the prime factors as well as the exponents of the factors, for example 28 = 2*2*7 = (2^2)*(7^1)
-            # we can compute all factors using the formula that uses their exponents
-            #  where a and b are exponents of subsequently higher prime factors:
-            # total num factors = (a + 1)* (b + 1) =(2 + 1)* (1 + 1) = 6
-            prime_factor_exponents = [value + 1 for _,value in prime_factors_dict.items()]
-            
-            total_factors = reduce(lambda x,y: x*y, prime_factor_exponents)
+            # Total factors are given by
+            total_factors = total_factor_calculator(prime_factor_odd)*total_factor_calculator(prime_factors_even)
 
             # Terminating condition if num factors is 500 or higher
             if total_factors >= 500:
@@ -60,7 +72,11 @@ def highly_divisible_triangular_number():
 
 
 if __name__ == "__main__":
+
+    start = time()
     print(brute_force())
-    # print(highly_divisible_triangular_number())
+    print("Brute force time:", time() - start)
 
-
+    start = time()
+    print(highly_divisible_triangular_number())
+    print("Optimized time:", time() - start)
